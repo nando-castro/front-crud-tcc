1e4ff6a256ad3c7fca617dfc32f8bcd0b9569359
nando-castro <fernando.castro10.master@gmail.com>
2023-10-02 16:34:12 -0300

    :sparkles: feat: arrumado build 5

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    externalDir: true | {
-        enabled: true,
-        silent: true,
-   },
-  }
 }
 
 
