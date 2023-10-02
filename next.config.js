c570af2fe43e3f711f582bc1a661742d0e3b2666
nando-castro <fernando.castro10.master@gmail.com>
2023-10-02 15:57:38 -0300

    :sparkles: feat: arrumado build 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    externalDir: true
+  },
+  // Potential new config flag:
+  disableExperimentalFeaturesWarning: true
 }
+
 
 module.exports = nextConfig 
