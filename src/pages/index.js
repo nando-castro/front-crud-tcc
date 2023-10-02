e9f1cfc8f9319256d6057fe8ae6464c3fd5dc85c
fernando <sousa.fernandocastro@gmail.com>
2023-10-02 11:15:06 -0300

    :sparkles: feat: implementado tabela de aplicação

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,54 @@
-import styles from '@/styles/Home.module.css'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [lista, setLista] = useState([
+    {
+      id: 1,
+      nome: "item1",
+      preco: 33,
+    },
+    {
+      id: 2,
+      nome: "item2",
+      preco: 55,
+    },
+  ]);
+
+  function renderizaItens() {
+    return lista.map((i) => (
+      <div className={styles.items}>
+        <p className={styles.item}>{i.nome}</p>
+        <p className={styles.item}>R$ {i.preco}</p>
+        <p className={styles.opcoes}>
+          <button className={styles.buttonVer} onClick={() => ver(i.id)}>Ver</button>
+          <button className={styles.buttonEditar} onClick={() => editar(i.id)}>Editar</button>
+          <button className={styles.buttonDeletar} onClick={() => deletar(i.id)}>Deletar</button>
+        </p>
+      </div>
+    ));
+  }
+
+  function deletar() {
+    alert("selecionou deletar");
+  }
+
+  function editar() {
+    alert("selecionou editar");
+  }
+
+  function ver() {
+    alert("selecionou ver");
+  }
+
+  function adicionar() {
+    alert("selecionou adicionar");
+  }
+
   return (
     <>
       <Head>
@@ -14,8 +58,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Minha Aplicação</h1>
+        <h1 className={styles.titulo}>Minha Aplicação</h1>
+        <div className={styles.tabela}>
+          <h2 className={styles.tituloTabela}>Lista</h2>
+          <div className={styles.items}>
+            <p className={styles.item}>Nome</p>
+            <p className={styles.item}>Preço (R$)</p>
+            <p className={styles.item}>Opções</p>
+          </div>
+          {renderizaItens()}
+        </div>
       </main>
     </>
-  )
+  );
 }
